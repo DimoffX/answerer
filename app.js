cabb2918407cc9ae24bc1e3a273d2fa96c77bdfa
Dimoff <ivan@dimoff.biz>
2016-06-08 22:49:50 +0300

    stuff

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require("express");
-var bodyParser = require("body-parser");
-var cookieParser = require("cookie-parser");
+//var bodyParser = require("body-parser");
+//var cookieParser = require("cookie-parser");
 var app = express();
 var request = require("request");
 var cheerio = require("cheerio");
 app.set('view engine', 'ejs');
-app.use(bodyParser());
-app.use(cookieParser());
+//app.use(bodyParser());
+//app.use(cookieParser());
 app.use(express.static('public'));
 
 
